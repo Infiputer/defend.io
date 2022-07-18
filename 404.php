ddddb0a86c28735362b330e45beb8aa5ea3d0f5e
AudioCypher <78041811+audiotonewastaken@users.noreply.github.com>
2022-07-18 11:05:46 -0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,8 +1,10 @@
 <html>
 	<body>
 		<h1><tt>
+			<h1>404</h1>
 		<div>Page not found</div>
 		<a href = "/">Go to Defend.io</a>
+			<p>Site may be under construction, please contact Infiputer for the status</p>
 		</tt></h1>
 	</body>
 </html>
