49947e99add31b68319106ed7c6a958e6e158ed2
Infiputer <anipatilYay@outlook.com>
2022-08-16 07:50:11 -0700

    formatted code

--- extra.js
+++ extra.js
@@ -19,25 +19,27 @@ var treeimg = new Image();
 treeimg.src = 'images/tree.svg';
 
 function setSize() {
-	c.width = window.innerWidth - 20;
-	c.height = window.innerHeight - 30;
-	width = window.innerWidth - 20;
-	height = window.innerHeight - 30;
-	maxLen = Math.max(width, height);
+  c.width = window.innerWidth - 20;
+  c.height = window.innerHeight - 30;
+  width = window.innerWidth - 20;
+  height = window.innerHeight - 30;
+  maxLen = Math.max(width, height);
 }
 setSize()
-window.onresize=setSize;
+window.onresize = setSize;
 
 mobileButtons = []
-function onMobileDown(x){
-	event.preventDefault()
-	mobileButtons.push(x)
+
+function onMobileDown(x) {
+  event.preventDefault()
+  mobileButtons.push(x)
 }
-function onMobileUp(x){
-	event.preventDefault()
-	mobileButtons = mobileButtons.filter(function(y){
-			return x!=y
-	})
+
+function onMobileUp(x) {
+  event.preventDefault()
+  mobileButtons = mobileButtons.filter(function(y) {
+    return x != y
+  })
 }
 
 let details = navigator.userAgent;
@@ -48,7 +50,7 @@ let regexp = /android|iphone|kindle|ipad/i;
 let isMobileDevice = regexp.test(details);
 
 if (isMobileDevice) {
-	document.getElementById("mobileButtons").innerHTML += `<style>
+  document.getElementById("mobileButtons").innerHTML += `<style>
 #buttonW{				
 	left: 10%;
 	bottom: 20%;
@@ -84,115 +86,121 @@ var ctx = c.getContext("2d");
 
 function keypress(event) {
   key = event.keyCode;
-	keymap[event.keyCode] = event.type == 'keydown';
-	for(k2 in keymap){
-		if(keymap[k2]==false){
-			delete keymap[k2];
-		}
-	}
+  keymap[event.keyCode] = event.type == 'keydown';
+  for (k2 in keymap) {
+    if (keymap[k2] == false) {
+      delete keymap[k2];
+    }
+  }
 }
 
-function constrain(val, min_val, max_val){
-    return Math.min(max_val, Math.max(min_val, val))
+function constrain(val, min_val, max_val) {
+  return Math.min(max_val, Math.max(min_val, val))
 }
 
 function mathmap(x, in_min, in_max, out_min, out_max) {
-		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 document.addEventListener('keyup', keypress);
 
-setTimeout(function(){
-	console.log(" ▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄      ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄           \n▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░▌    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌          \n▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀█░▌    ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌          \n▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌          ▐░▌▐░▌    ▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌          \n▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌ ▐░▌   ▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌          \n▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌          \n▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌   ▐░▌ ▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌          \n▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌          ▐░▌    ▐░▌▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌          \n▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌          ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄█░▌ ▄  ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌          \n▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░▌ ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌          \n ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀           \n                                                                                                                     \n ▄▄▄▄▄▄▄▄▄▄   ▄         ▄                                                                                            \n▐░░░░░░░░░░▌ ▐░▌       ▐░▌                                                                                           \n▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌                                                                                           \n▐░▌       ▐░▌▐░▌       ▐░▌                                                                                           \n▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌                                                                                           \n▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌                                                                                           \n▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀                                                                                            \n▐░▌       ▐░▌     ▐░▌                                                                                                \n▐░█▄▄▄▄▄▄▄█░▌     ▐░▌                                                                                                \n▐░░░░░░░░░░▌      ▐░▌                                                                                                \n ▀▀▀▀▀▀▀▀▀▀        ▀                                                                                                 \n                                                                                                                     \n ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ \n▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌\n ▀▀▀▀█░█▀▀▀▀ ▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌\n     ▐░▌     ▐░▌▐░▌    ▐░▌▐░▌               ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     ▐░▌          ▐░▌       ▐░▌\n     ▐░▌     ▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌\n     ▐░▌     ▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌\n     ▐░▌     ▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀▀▀      ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌     ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ \n     ▐░▌     ▐░▌    ▐░▌▐░▌▐░▌               ▐░▌     ▐░▌          ▐░▌       ▐░▌     ▐░▌     ▐░▌          ▐░▌     ▐░▌  \n ▄▄▄▄█░█▄▄▄▄ ▐░▌     ▐░▐░▌▐░▌           ▄▄▄▄█░█▄▄▄▄ ▐░▌          ▐░█▄▄▄▄▄▄▄█░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌ \n▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░▌          ▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌\n ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀            ▀▀▀▀▀▀▀▀▀▀▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀ ")
+setTimeout(function() {
+  console.log(" ▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄      ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄           \n▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░▌    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌          \n▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀█░▌    ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌          \n▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌          ▐░▌▐░▌    ▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌          \n▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌ ▐░▌   ▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌          \n▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌          \n▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌   ▐░▌ ▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌          \n▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌          ▐░▌    ▐░▌▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌          \n▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌          ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄█░▌ ▄  ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌          \n▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░▌ ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌          \n ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀           \n                                                                                                                     \n ▄▄▄▄▄▄▄▄▄▄   ▄         ▄                                                                                            \n▐░░░░░░░░░░▌ ▐░▌       ▐░▌                                                                                           \n▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌                                                                                           \n▐░▌       ▐░▌▐░▌       ▐░▌                                                                                           \n▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌                                                                                           \n▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌                                                                                           \n▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀                                                                                            \n▐░▌       ▐░▌     ▐░▌                                                                                                \n▐░█▄▄▄▄▄▄▄█░▌     ▐░▌                                                                                                \n▐░░░░░░░░░░▌      ▐░▌                                                                                                \n ▀▀▀▀▀▀▀▀▀▀        ▀                                                                                                 \n                                                                                                                     \n ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ \n▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌\n ▀▀▀▀█░█▀▀▀▀ ▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌\n     ▐░▌     ▐░▌▐░▌    ▐░▌▐░▌               ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     ▐░▌          ▐░▌       ▐░▌\n     ▐░▌     ▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌\n     ▐░▌     ▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌\n     ▐░▌     ▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀▀▀      ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌     ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ \n     ▐░▌     ▐░▌    ▐░▌▐░▌▐░▌               ▐░▌     ▐░▌          ▐░▌       ▐░▌     ▐░▌     ▐░▌          ▐░▌     ▐░▌  \n ▄▄▄▄█░█▄▄▄▄ ▐░▌     ▐░▐░▌▐░▌           ▄▄▄▄█░█▄▄▄▄ ▐░▌          ▐░█▄▄▄▄▄▄▄█░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌ \n▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░▌          ▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌\n ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀            ▀▀▀▀▀▀▀▀▀▀▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀ ")
 }, 500)
 sounddict = {};
-function playSound(filename){
-	soundeffectAudio = new Audio('/sounds/'+filename);
-	soundeffectAudio.play();
+
+function playSound(filename) {
+  soundeffectAudio = new Audio('/sounds/' + filename);
+  soundeffectAudio.play();
 }
 
 
 
 let interval;
 
-if (!('ongamepadconnected'in window)) {
-		// No gamepad events available, poll instead.
-		interval = setInterval(pollGamepads, 500);
+if (!('ongamepadconnected' in window)) {
+  // No gamepad events available, poll instead.
+  interval = setInterval(pollGamepads, 500);
 }
 
 function pollGamepads() {
-		const gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
-		for (let i = 0; i < gamepads.length; i++) {
-				const gp = gamepads[i];
-				if (gp) {
-					 console.log("Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.");
-						gameLoop();
-						clearInterval(interval);
-				}
-		}
+  const gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+  for (let i = 0; i < gamepads.length; i++) {
+    const gp = gamepads[i];
+    if (gp) {
+      console.log("Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.");
+      gameLoop();
+      clearInterval(interval);
+    }
+  }
 }
+
 function getDistance(x1, y1, x2, y2) {
-		let y = x2 - x1;
-		let x = y2 - y1;
-		return Math.sqrt(x * x + y * y);
+  let y = x2 - x1;
+  let x = y2 - y1;
+  return Math.sqrt(x * x + y * y);
 }
-controllerMoved=true;
+controllerMoved = true;
+
 function gameLoop() {
-		const gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
-		if (!gamepads) {
-				return;
-		}
-		playerX+=gamepads[0].axes[0]*speed;
-		playerY+=gamepads[0].axes[1]*speed;
-		controllerMoved=true;
-		mouseX = gamepads[0].axes[2]*width/4+width/2;
-		mouseY = gamepads[0].axes[3]*height/4+height/2;
-		if(gamepads[0].buttons[7].pressed){
-			launchArrow();
-		}
-		if(gamepads[0].buttons[6].pressed){
-			speed=4;
-		}
-		else{
-			speed=2;
-		}
-		direction = Math.atan2(
-			mouseY-(height/2),
-			mouseX-(width/2),
-		)
-		start = requestAnimationFrame(gameLoop);
+  const gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
+  if (!gamepads) {
+    return;
+  }
+  playerX += gamepads[0].axes[0] * speed;
+  playerY += gamepads[0].axes[1] * speed;
+  controllerMoved = true;
+  mouseX = gamepads[0].axes[2] * width / 4 + width / 2;
+  mouseY = gamepads[0].axes[3] * height / 4 + height / 2;
+  if (gamepads[0].buttons[7].pressed) {
+    launchArrow();
+  }
+  if (gamepads[0].buttons[6].pressed) {
+    speed = 4;
+  } else {
+    speed = 2;
+  }
+  direction = Math.atan2(
+    mouseY - (height / 2),
+    mouseX - (width / 2),
+  )
+  start = requestAnimationFrame(gameLoop);
 }
-setInterval(function(){
-	if(controllerMoved){
-		controllerMoved=false;
-		moveMe(playerX, playerY, direction);
-	}
+setInterval(function() {
+  if (controllerMoved) {
+    controllerMoved = false;
+    moveMe(playerX, playerY, direction);
+  }
 }, 100)
 
-function circle(x, y, radius, color){
-	ctx.fillStyle = color;
-	ctx.beginPath();
-	ctx.arc(x, y, radius, 0, 2 * Math.PI);
-	ctx.fill();
+function circle(x, y, radius, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.fill();
 }
 
 function rect(x, y, height, width, color) {
-		ctx.fillStyle = color;
-		ctx.fillRect((x - width / 2), (y - height / 2), width, height)
+  ctx.fillStyle = color;
+  ctx.fillRect((x - width / 2), (y - height / 2), width, height)
 }
-shake1=-1;
-shake2=-1;
-function shakeScreen(){
-	clearInterval(shake1);
-	clearInterval(shake2);
-    shake1 = setInterval(function(){
-			document.getElementById("myCanvas").style.transform="rotateZ("+0.1+"deg)";
-		}, 100)
-    setTimeout(function(){
-        shake2 = setInterval(function(){
-					document.getElementById("myCanvas").style.transform="rotateZ("+(-0.1)+"deg)";
-				}, 100)
-    }, 50);
-    setTimeout(function(){clearInterval(shake1); clearInterval(shake2); document.getElementById("myCanvas").style.transform="rotateZ(0deg)";}, 200);
-}
+shake1 = -1;
+shake2 = -1;
 
+function shakeScreen() {
+  clearInterval(shake1);
+  clearInterval(shake2);
+  shake1 = setInterval(function() {
+    document.getElementById("myCanvas").style.transform = "rotateZ(" + 0.1 + "deg)";
+  }, 100)
+  setTimeout(function() {
+    shake2 = setInterval(function() {
+      document.getElementById("myCanvas").style.transform = "rotateZ(" + (-0.1) + "deg)";
+    }, 100)
+  }, 50);
+  setTimeout(function() {
+    clearInterval(shake1);
+    clearInterval(shake2);
+    document.getElementById("myCanvas").style.transform = "rotateZ(0deg)";
+  }, 200);
+}
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-Hours_Wasted_On_This_Useless_Project=Infinity 
+Hours_Wasted_On_This_Useless_Project = Infinity
 playerX = 0
 playerY = 0
 
 money = 100;
 
 var keymap = {}
-hasMoved=true
+hasMoved = true
 learderboard = {}
 
 canshoot = true;
 
-var mouseX = width/2;
-var mouseY = height/2;
+var mouseX = width / 2;
+var mouseY = height / 2;
 
 shootangle = 0
 
@@ -21,78 +21,80 @@ speed = 2
 
 maxLen = Math.max(width, height)
 
-setInterval(function(){
+setInterval(function() {
 
-	rect(mouseX, mouseY, 10, 10, "black")
-	moveMe(playerX, playerY, direction)
-	
+  rect(mouseX, mouseY, 10, 10, "black")
+  moveMe(playerX, playerY, direction)
+
 }, 5000)
 
-setInterval(function(){
-	if(hasMoved){
-		hasMoved=false
-		moveMe(playerX, playerY, direction)
-	}
+setInterval(function() {
+  if (hasMoved) {
+    hasMoved = false
+    moveMe(playerX, playerY, direction)
+  }
 }, 50)
 
 document.addEventListener("mousemove", () => {
-  mouseX = event.clientX; 
-  mouseY = event.clientY; 
-	
-	direction = Math.atan2(
-		mouseY-(height/2),
-		mouseX-(width/2),
-	)
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+
+  direction = Math.atan2(
+    mouseY - (height / 2),
+    mouseX - (width / 2),
+  )
 
 });
 
-function launchArrow(){
-	if(canshoot){
-		socket.send(
-			"arrow "+(playerX+Math.cos(direction)*100)+","+(playerY+Math.sin(direction)*100)+","+direction
-		)
-		shakeScreen();
-		canshoot = false
-		shootangle+=0.05
-		setTimeout(function(){
-			canshoot = true
-		}, 100)
-	}
+function launchArrow() {
+  if (canshoot) {
+    socket.send(
+      "arrow " + (playerX + Math.cos(direction) * 100) + "," + (playerY + Math.sin(direction) * 100) + "," + direction
+    )
+    shakeScreen();
+    canshoot = false
+    shootangle += 0.05
+    setTimeout(function() {
+      canshoot = true
+    }, 100)
+  }
 }
 
 
 
 canMakeWall = true;
-function checkkeys(){
-	if(kdown(69)){
-		makeWall();
-	}
-	if(kdown(38) || kdown(87)){
-		playerY-=speed;
-		hasMoved = true;
-	}
-	if(kdown(40) || kdown(83)){
-		playerY+=speed;
-		hasMoved = true;
-	}
-	if(kdown(37) || kdown(65)){
-		playerX-=speed;
-		hasMoved = true;
-	}
-	if(kdown(39) || kdown(68)){
-		playerX+=speed;
-		hasMoved = true;
-	}
-	playerX=constrain(playerX, -5000, 5000);
-	playerY=constrain(playerY, -5000, 5000);
+
+function checkkeys() {
+  if (kdown(69)) {
+    makeWall();
+  }
+  if (kdown(38) || kdown(87)) {
+    playerY -= speed;
+    hasMoved = true;
+  }
+  if (kdown(40) || kdown(83)) {
+    playerY += speed;
+    hasMoved = true;
+  }
+  if (kdown(37) || kdown(65)) {
+    playerX -= speed;
+    hasMoved = true;
+  }
+  if (kdown(39) || kdown(68)) {
+    playerX += speed;
+    hasMoved = true;
+  }
+  playerX = constrain(playerX, -5000, 5000);
+  playerY = constrain(playerY, -5000, 5000);
 }
-function canvas_click(){
-	event.preventDefault();
+
+function canvas_click() {
+  event.preventDefault();
   event.stopPropagation();
-	launchArrow();
+  launchArrow();
 }
 setInterval(checkkeys, 10)
 
-function kdown(k){
-	return keymap[k];
+function kdown(k) {
+  return keymap[k];
 }
